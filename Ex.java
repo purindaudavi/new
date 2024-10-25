b7ee8c0ee72921081039b17a3bdd90255eec37c9
purindaudavi <115772017+purindaudavi@users.noreply.github.com>
2024-10-25 12:47:46 +0530

    Add files via upload

---
+++ Ex.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+import java.util.SequencedCollection;
+
+
+public class Ex {
+    public Ex(){
+        SequencedCollection sc = new LinkedList();
+        sc.addFirst(5);
+        sc.addFirst(2);
+        sc.addLast(3);
+        System.out.println(sc.getFirst());
+        System.out.println(sc.removeLast());
+        System.out.println(sc);
+        System.out.println(sc.reversed());
+
+    }
+    public static void main(String[] args) {
+        new Ex();
+    }
+}
